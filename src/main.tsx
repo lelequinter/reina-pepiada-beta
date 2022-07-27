ee965b57c30361b9d8c3a89ea79becf0c698c6a4
Leyder Quintero <lele.quinter@gmail.com>
2022-07-27 17:10:58 -0500

    Inicio de protecto y Navbar provicional

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { Navbar } from './ui'
-// import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import { ReinaApp } from "./ReinaApp";
 
-
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <Navbar />
+    <BrowserRouter>
+      <ReinaApp />
+    </BrowserRouter>
   </React.StrictMode>
-)
+);
--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,6 +1,22 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+import { Home } from "../main/pages/Home";
+import { Navbar } from "../ui";
+
 
 export const AppRouter = () => {
   return (
-    <div>AppRouter</div>
-  )
-}
+    <>
+      <Navbar />
+
+      <Routes>
+        {/* Rutas de Heroes */}
+        <Route path="home" element={<Home />} />
+        {/* <Route path="dc" element={<DcPage />} /> */}
+        {/* Rutas de Login */}
+        {/* <Route path="login" element={<LoginPage />} /> */}
+        {/* Rutas Default */}
+        <Route path="/" element={<Navigate to={"/home"} />} />
+      </Routes>
+    </>
+  );
+};
